3032f8101b6b2ecf7a19085f37fde0c70fcd5f0e
rajukundan <57005238+rajukundan@users.noreply.github.com>
2019-12-31 06:06:07 -0500

    Add files via upload
    added Progress bars and statement section

--- js/script.js
+++ js/script.js
@@ -10,7 +10,22 @@ $(function() { //this is document.ready method of jscript written in jquery form
     loop:true,
     autoplayHoverPause:true,
     nav:true,
-    dots:false,
-    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+  //  dots:false,
+  //  navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
   });
 })
+
+/* progress Bar */
+$(function () {
+
+    $("#progress-elements").waypoint(function () {
+      $(".progress-bar").each(function () {
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 2000);
+        });
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+    });
+});
